cd8d59ed9c041c62b3a5931a247a812f228e6b7d
Riccardo Rolfini <riccardo.rolfini@mexage.net>
2021-12-30 18:53:26 +0100

    Hello world

---
+++ CinemaRolfoBot/CinemaRolfoBot/Const.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CinemaRolfoBot
+{
+    public class Const
+    {
+        /// <summary>
+        /// La variabile d'ambiente in cui deve essere impostato il token ottenuto da @BotFather
+        /// </summary>
+        public const string ENV_VAR_KEY_TOKEN = "TOKEN";
+    }
+}
---
+++ CinemaRolfoBot/CinemaRolfoBot/Program.cs
@@ -0,0 +1,16 @@
+﻿using CinemaRolfoBot;
+using Telegram.Bot;
+
+string? token = Environment.GetEnvironmentVariable(Const.ENV_VAR_KEY_TOKEN);
+if (string.IsNullOrEmpty(token))
+{
+    Console.WriteLine($"La variabile d'ambiente {Const.ENV_VAR_KEY_TOKEN} non risulta impostata.");
+    return -1;
+}
+
+var telegramBotClient = new TelegramBotClient(token);
+
+var me = await telegramBotClient.GetMeAsync();
+Console.WriteLine($"Hello, World! Sono l'utente {me.Id} e il mio nome è {me.FirstName}.");
+
+return 0;
