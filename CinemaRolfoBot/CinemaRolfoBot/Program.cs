9765043f2c8c0674e24c56f122aff8efcb6e247d
Riccardo Rolfini <riccardo.rolfini@mexage.net>
2021-12-30 23:09:15 +0100

    Aggiunto metodo che lancia aggiornamento DB a intervalli regolari

--- CinemaRolfoBot/CinemaRolfoBot/Program.cs
+++ CinemaRolfoBot/CinemaRolfoBot/Program.cs
@@ -2,6 +2,7 @@
 using CinemaRolfoBot.Model.DB;
 using CinemaRolfoBot.Model.Json;
 using System.Text.Json;
+using System.Text.RegularExpressions;
 using Telegram.Bot;
 
 string? Token = Environment.GetEnvironmentVariable(Const.ENV_VAR_KEY_TOKEN);
@@ -21,22 +22,13 @@ if (string.IsNullOrEmpty(PostgresConnectionString))
 TelegramBotClient telegramBotClient = await InitBot(Token);
 CinemaContext context = InitDB(PostgresConnectionString);
 
-try
-{
-    HttpClient client = new HttpClient();
-    string responseBody = await client.GetStringAsync(Const.TSB_FILMS_WITH_SHOWING_URL);
+CancellationToken CancellationToken = new CancellationToken();
+Task task = PeriodicUpdateDB(TimeSpan.FromMinutes(5), CancellationToken);
 
-    FilmsWithShowings? filmsWithShowings = JsonSerializer.Deserialize<FilmsWithShowings>(responseBody);
+while (true)
+    await Task.Delay(TimeSpan.FromMinutes(1), CancellationToken);
 
-    Console.WriteLine(filmsWithShowings);
-}
-catch (HttpRequestException e)
-{
-    Console.WriteLine("\nException Caught!");
-    Console.WriteLine("Message :{0} ", e.Message);
-}
-
-return 0;
+return -1;
 
 async Task<TelegramBotClient> InitBot(string Token)
 {
@@ -52,4 +44,52 @@ CinemaContext InitDB(string PostgresConnectionString)
 {
     CinemaContext Context = new CinemaContext(PostgresConnectionString);
     return Context;
+}
+
+async Task PeriodicUpdateDB(TimeSpan interval, CancellationToken cancellationToken)
+{
+    string lastFilmsWithShowing = "";
+    while (true)
+    {
+        lastFilmsWithShowing = await UpdateDB(lastFilmsWithShowing);
+
+        await Task.Delay(interval, cancellationToken);
+        if (cancellationToken.IsCancellationRequested)
+            break;
+    }
+}
+
+async Task<string> UpdateDB(string lastFilmsWithShowing)
+{
+    string responseBody = "";
+    try
+    {
+        HttpClient client = new();
+        responseBody = await client.GetStringAsync(Const.TSB_FILMS_WITH_SHOWING_URL);
+    }
+    catch (HttpRequestException e)
+    {
+        //TODO Notify errors to mantainers users
+        Console.WriteLine($"Errore nella richiesta HTTP verso {Const.TSB_FILMS_WITH_SHOWING_URL}. Error message: {e.Message}");
+        return lastFilmsWithShowing;
+    }
+
+    //Comparing with previous responseBody
+    string currentFilmsWithShowings = Regex.Replace(responseBody, @"\s+", String.Empty);
+    if (lastFilmsWithShowing == currentFilmsWithShowings)
+        return currentFilmsWithShowings;
+
+    FilmsWithShowings? filmsWithShowings = null;
+    try
+    {
+        filmsWithShowings = JsonSerializer.Deserialize<FilmsWithShowings>(responseBody);
+    }
+    catch (JsonException e)
+    {
+        //TODO Notify errors to mantainers users
+        Console.WriteLine($"Errore nel parsing JSON della response da {Const.TSB_FILMS_WITH_SHOWING_URL}. Error message: {e.Message}");
+        return lastFilmsWithShowing;
+    }
+
+    return currentFilmsWithShowings;
 }
