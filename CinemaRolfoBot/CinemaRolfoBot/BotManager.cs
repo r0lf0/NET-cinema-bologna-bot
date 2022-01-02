b1f4dcfe3eae9f2c638773e28af6cf492ff411cd
Riccardo Rolfini <rolfo1993@gmail.com>
2022-01-02 19:14:54 +0100

    Help cmd and error falling added to BotManager. Bonus: EmojiParser

--- CinemaRolfoBot/CinemaRolfoBot/BotManager.cs
+++ CinemaRolfoBot/CinemaRolfoBot/BotManager.cs
@@ -33,21 +33,22 @@ namespace CinemaRolfoBot
 
         private async Task HandleUpdateAsync(ITelegramBotClient botClient, Update update, CancellationToken cancellationToken)
         {
-            if (update.Type != UpdateType.Message)
-                return;
-            if (update.Message!.Type != MessageType.Text)
-                return;
+            var chatId = update?.Message?.Chat?.Id;
+            var messageText = update?.Message?.Text;
+            Log.Debug($"Received '{messageText}' message in chat {chatId}.");
 
-            var chatId = update.Message.Chat.Id;
-            var messageText = update.Message.Text;
+            if (chatId == null)
+                Log.Error("Received message without ChatId.");
+            else if (update.Type != UpdateType.Message
+                         || update.Message!.Type != MessageType.Text
+                         || string.IsNullOrWhiteSpace(messageText))
+                _ = SendErrorMessageWithHelpRedirect(chatId);
+            else if (Commands_Help.Contains(messageText, StringComparer.OrdinalIgnoreCase))
+                _ = HandleHelpCmd(chatId, update.Message);
+            else
+                _ = SendErrorMessageWithHelpRedirect(chatId);
 
-            Console.WriteLine($"Received a '{messageText}' message in chat {chatId}.");
-
-            // Echo received message text
-            Message sentMessage = await botClient.SendTextMessageAsync(
-                chatId: chatId,
-                text: "You said:\n" + messageText,
-                cancellationToken: cancellationToken);
+            return;
         }
 
         private Task HandleErrorAsync(ITelegramBotClient botClient, Exception exception, CancellationToken cancellationToken)
@@ -62,5 +63,37 @@ namespace CinemaRolfoBot
             Console.WriteLine(ErrorMessage);
             return Task.CompletedTask;
         }
+
+        private async Task<string> SendErrorMessageWithHelpRedirect(ChatId chatId)
+        {
+            Message sentMessage = await telegramBotClient.SendTextMessageAsync(
+                chatId: chatId,
+                text: EmojiParser.ReplaceColonNames(Msg_Error));
+            return sentMessage.Text;
+        }
+
+        private async Task<string> HandleHelpCmd(ChatId chatId, Message message)
+        {
+            Message sentMessage = await telegramBotClient.SendTextMessageAsync(
+                chatId: chatId,
+                text: Msg_Welcome);
+            return sentMessage.Text;
+        }
+
+        private static readonly IEnumerable<string> Commands_Help = new List<string> { "/aiuto", "/help", "/start" };
+        private static readonly IEnumerable<string> Commands_TodayShowings = new List<string> { "/todayshowings" };
+        private static readonly IEnumerable<string> Commands_DailyShowingsAll = new List<string> { "/completeshowings" };
+        private static readonly IEnumerable<string> Commands_FilmDetails = new List<string> { "/film" };
+        private static readonly IEnumerable<string> Commands_WhichTime = new List<string> { "/ora" };
+
+        private const string Msg_Welcome = "Ciao, sono il bot non ufficiale del The Space di Bologna." +
+                                           "Ecco le cose che puoi chiedermi:" +
+                                           "\n/aiuto per rileggere questo messaggio' " +
+                                           "\n/showPerData per ricevere la programmazione per data' " +
+                                           "\n/film per la lista dei film in programmazione' " +
+                                           "\n/film seguito da parte del titolo di un film per conoscerne i dettagli";
+
+        private const string Msg_Error = "Scusa, non ho capito... :sob:" +
+                                         "\nPer il momento accetto solo i comandi riportati qui: /aiuto";
     }
 }
